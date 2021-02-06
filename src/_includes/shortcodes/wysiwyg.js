4b814fa05ba424d3c7d7a06369b0648b90316406
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-02-06 16:06:47 +0100

    Add timeline basic setup

--- src/_includes/shortcodes/wysiwyg.js
+++ src/_includes/shortcodes/wysiwyg.js
@@ -1,4 +1,8 @@
-module.exports = nunjucksEnvironment => (html, { quotes, content }) => {
+module.exports = nunjucksEnvironment => (
+  html,
+  { quotes, content, timeline }
+) => {
+  console.log('timeline: ', timeline)
   const container = component =>
     `</section><div class="text-page__breakout">${component}</div><section class="text-page__content">`
 
@@ -6,8 +10,14 @@ module.exports = nunjucksEnvironment => (html, { quotes, content }) => {
     container("{% include 'module/quote-list.njk' %}"),
     quotes
   )
+  const timelineComponent = nunjucksEnvironment.renderString(
+    container("{% include 'module/timeline.njk' %}"),
+    timeline
+  )
 
-  const rendered = html.replace('[testimonials]', quotesComponent)
+  const rendered = html
+    .replace('[testimonials]', quotesComponent)
+    .replace('[timeline]', timelineComponent)
 
   return rendered
 }
